7e86d7aa047487657a88b5212719b9830e07ad14
IotaSpencer <me@iotaspencer.me>
2025-01-25 05:00:00 -0500

    Update configuration to use toml library and adjust dependencies

--- clickflare/config.py
+++ clickflare/config.py
@@ -1,4 +1,4 @@
-import tomllib
+import toml
 import pathlib
 
 
@@ -6,7 +6,7 @@ class Config:
   def __init__(self):
     cfg_path = pathlib.Path.home() / '.cfcli/config'
     cfg = None
-    with open(cfg_path, 'rb') as cfg_file:
-      cfg = tomllib.load(cfg_file)
+    with open(cfg_path, 'r') as cfg_file:
+      cfg = toml.load(cfg_file)
     
     self.token = cfg['cfcli-api']['token']
